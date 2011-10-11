90e72865cd0e8dd05b8794a51c4ca246102c7a40
heavenslashinghell@gmail.com <heavenslashinghell@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2011-10-11 14:04:15 +0000

    Added Directory and File

---
+++ trunk/001 Test/Linklist.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct Foo { int data; node *tail; node *head; } node;
+
+void print_list(node *pointer);
+int insert_front(node *pointer, int number);
+int insert_back(node *pointer);
+int free_list(node *pointer);
+
+
+
+int main(){
+}
