8dd642f0e154108012dbcda9c113197ce8bae880
heavenslashinghell@gmail.com <heavenslashinghell@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2011-10-12 17:18:16 +0000

    Fixed Dangling Pointers, function starts off with a 0 root node. See if fix is needed, otherwise - code is done.

--- trunk/001 Test/Linklist.cpp
+++ trunk/001 Test/Linklist.cpp
@@ -19,7 +19,6 @@ dbNode* freeOne(dbNode *pointer);
 int listLength(dbNode *pointer);
 
 
-
 int main(){
   dbNode *address = new dbNode();
 
@@ -28,12 +27,12 @@ int main(){
 
   cout << "Doubly-Linked List length is " << listLength(address) << endl;
   printAll(address);
-  printOne(address -> tail -> tail);                // 1-2-3, prints value at 3
-  printOne(address -> tail -> head);                // 1-2-3, prints value at 1
-  printOne(address -> tail);                        // 1-2-3,prints value at 2
+  printOne(address -> tail -> tail);                // 2-1-0, prints value at 0
+  printOne(address -> tail -> head);                // 2-1-0, prints value at 2
+  printOne(address -> tail);                        // 2-1-0,prints value at 1
   freeOne(address -> tail);
-  printOne(address -> tail);                        // What does 1 think is tailing it? 2 or 3?
-  printOne(address -> tail -> head);        // What does 3 think is before it? 1 or 2?
+  printOne(address -> tail);                        // What does 2 think is tailing it? 1 or 0?
+  printOne(address -> tail -> head);                // What does 0 think is before it? 2 or 1?
 
   freeAll(address);
   printAll(address);
@@ -89,12 +88,12 @@ dbNode* freeAll(dbNode *pointer){
   while(pointer->head){
     pointer = pointer -> head;
   }
-
-  dbNode *ptr  = pointer;
   while(pointer){
+    dbNode *ptr = new dbNode();
+    ptr = pointer;
     pointer = pointer -> tail;
     delete ptr;
-    ptr = pointer;
+    ptr = NULL;
   }
 
   return pointer;
@@ -104,7 +103,6 @@ dbNode* freeOne(dbNode *pointer){
   (pointer -> head) -> tail = pointer -> tail;
   (pointer -> tail) -> head = pointer -> head;
   delete pointer;
-  pointer = NULL;
 
   return pointer;
 }
