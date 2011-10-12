ab4e252636923d2cac9a89a155c250975de4ee86
heavenslashinghell@gmail.com <heavenslashinghell@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2011-10-12 16:37:33 +0000

    Tail node of Freed Node has a dangling pointer for Head. Fix later.

--- 001 Test/Linklist.cpp
+++ 001 Test/Linklist.cpp
@@ -21,11 +21,25 @@ int listLength(dbNode *pointer);
 
 
 int main(){
-  dbNode *address = (dbNode*)malloc(sizeof(dbNode));
-  address = NULL;
- 
-  address -> head = insertFront(address, 10);
-  address = address -> head;
+  dbNode *address = new dbNode();
+
+  address = insertFront(address, 1);
+  address = insertFront(address, 2);
+
+  cout << "Doubly-Linked List length is " << listLength(address) << endl;
+  printAll(address);
+  printOne(address -> tail -> tail);                // 1-2-3, prints value at 3
+  printOne(address -> tail -> head);                // 1-2-3, prints value at 1
+  printOne(address -> tail);                        // 1-2-3,prints value at 2
+  freeOne(address -> tail);
+  printOne(address -> tail);                        // What does 1 think is tailing it? 2 or 3?
+  printOne(address -> tail -> head);        // What does 3 think is before it? 1 or 2?
+
+  freeAll(address);
+  printAll(address);
+
+  int i;
+  cin >> i;
 }
 
 void printOne(dbNode *point){
@@ -34,7 +48,7 @@ void printOne(dbNode *point){
 
 void printList(dbNode *point){
   while(point){
-    cout << point -> data << ", ";
+    cout << (point->data) << ", ";
     point = point -> tail;
   }
   point?cout << "SegFault\n" : cout << "Null\n";
@@ -44,22 +58,30 @@ void printAll(dbNode *point){
   while(point->head){
     point = point -> head;
   }
-  printList(point);
+  while(point){
+      cout << (point-> data) << ", ";
+      point = point -> tail;
+  }
+  point?cout << "SegFault\n" : cout << "Null\n";
 }
 
 dbNode* insertFront(dbNode *point, int num){
-  dbNode* snode = (dbNode*)malloc(sizeof(dbNode));
+  dbNode* snode = new dbNode();
   snode -> data = num;
   snode -> tail = point;
   snode -> head = point -> head;
+
+  point -> head = snode;
   return snode;
 }
 
 dbNode* insertBack(dbNode *point, int num){
-  dbNode *snode = (dbNode*)malloc(sizeof(dbNode));
+  dbNode *snode = new dbNode();
   snode -> data = num;
   snode -> tail = point -> tail;
   snode -> head = point;
+
+  point -> head = snode;
   return snode;
 }
 
@@ -67,23 +89,23 @@ dbNode* freeAll(dbNode *pointer){
   while(pointer->head){
     pointer = pointer -> head;
   }
- 
+
   dbNode *ptr  = pointer;
   while(pointer){
     pointer = pointer -> tail;
-    free(ptr);
+    delete ptr;
     ptr = pointer;
   }
- 
+
   return pointer;
 }
 
 dbNode* freeOne(dbNode *pointer){
   (pointer -> head) -> tail = pointer -> tail;
   (pointer -> tail) -> head = pointer -> head;
-  free(pointer);
+  delete pointer;
   pointer = NULL;
- 
+
   return pointer;
 }
 
@@ -93,8 +115,9 @@ int listLength(dbNode *pointer){
   }
   int i = 0;
   while (pointer){
+    pointer = pointer->tail;
     i++;
   }
- 
+
   return i;
 }
