07a7d58359422574e534edae76d684fb2740f773
heavenslashinghell@gmail.com <heavenslashinghell@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2012-01-12 16:46:55 +0000

    Updated servant.c

--- Torrent/servant2.c
+++ Torrent/servant2.c
@@ -1,28 +1,74 @@
 #include "servant.h"
 
 int main(){
+  bootUp();
+  initialize();
+  peerConnect();
+  listenIn();
+
+  exit(EXIT_SUCCESS);
 }
 
 void error(const char *msg){
-  perror(msg);
-  exit(EXIT_FAILURE);
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
 void bootUp(){
-  if((selfSock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-    error("BootUp: Socket creation failed");
-  }
+    if((selfIn = socket(AF_INET, SOCK_STREAM, 0)) != SUCCESS){
+        error("BootUp: Server-Purpose Socket Creation Failed");
+    }else{
+        printf("Server-Purpose Socket Descriptor: %d\n", selfIn);
+    }
 
-  struct sockaddr_in selfAddr;
-  memset(&selfAddr, 0, sizeof(selfAddr));
-  selfAddr.sin_family = AF_INET;
-  selfAddr.sin_addr.s_addr = INADDR_ANY;
-  selfAddr.sin_port = htons(DEFAULT_PORT);
-  int socklen = sizeof(sock);
-  if(bind(selfSock, (struct sockaddr *)&sock, socklen) == -1){
-    error("BootUp: Binding failed");
-  }
+    memset(&selfAddr, 0, sizeof(selfAddr));
+    selfAddr.sin_family = AF_INET;
+    selfAddr.sin_addr.s_addr = INADDR_ANY;
+    selfAddr.sin_port = htons(DEFAULT_PORT);
+    int socklen = sizeof(selfAddr);
+    if(bind(selfIn, (struct sockaddr *)&selfAddr, socklen) != SUCCESS){
+        error("BootUp: Binding Failed");
+    }
+
+    if((selfOut = socket(AF_INET, SOCK_STREAM, 0)) != SUCCESS){
+        error("BootUp: Client-Purpose Socket creation failed");
+    }else{
+        printf("Client-Purpose Socket Descriptor: %d\n", selfOut);
+    }
 }
 
 void initialize(){
+    if(int centralServer = connect(selfOut, (struct sockaddr *)&selfOut, sizeof(selfOut)) != SUCCESS){
+        error("Initialize: Connection To Central Server Failed");
+    }else{
+        printf("Connected to central server\n");
+    }
+
+    // retrieve data from central server
+    char buffer[BUFFER_MAX] = "Requesting List";
+    int bytes = write(selfOut, buffer, strlen(buffer) + 1);
+
+    bytes = read(selfOut, buffer, strlen(buffer));
+    printf("\tReceived: %s\n", buffer);
+
+    // process the buffer into a list
+}
+
+void peerConnect(){
+  int i;
+  for(i = 0; i < MAX_CLIENT; i++){
+    struct sockaddr_in sock;
+    sock.sin_family = AF_INET;
+    inet_aton(onlineList[i].addr, &sock.sin_addr);
+    sock.sin_port = htons(DEFAULT_PORT);
+    if(connect(selfOut, (struct sockaddr *)&sock, sizeof(sock)) != SUCCESS){
+      error("PeerConnect: Connect to peer clients failed");
+    }else{
+      printf("Connected to %s\n", onlineList[i].addr);
+    }
+  }
+}
+
+void listenIn(){
+  // server listens and accepts connections
 }
