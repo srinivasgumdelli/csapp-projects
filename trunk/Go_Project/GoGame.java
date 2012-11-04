17ed5cc1d5e426ad61522b478489d72cfd72c3ab
maxwizdomer@gmail.com <maxwizdomer@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2012-11-04 03:50:54 +0000

    commit

--- trunk/Go_Project/GoGame.java
+++ trunk/Go_Project/GoGame.java
@@ -1,6 +1,15 @@
 public class GoGame
 {
     private Goban _board;
+    private int _turns;
+    private GameTreenode current;
+    private GameTreenode root;
 
+    public GoGame()
+    {
+	_board = new Goban();
+	turns = 0;
+	root = current = new GameTreenode;
+    }
 
 }
--- trunk/Go_Project/Goban.java
+++ trunk/Go_Project/Goban.java
@@ -1,6 +1,34 @@
-public class GoGame
+public class Goban
 {
 
+    Location[][] board = new Location[19][19];
 
+    public Goban()
+    {
+	for(int i = 0; i < Location.length; i++)
+	    for(int j = 0; j < Location[0].length; j++)
+		Location[i][j] = new Location();
+    }
+
+
+    public boolean playStone(Color c, Location l)
+    {
+	boolean ret;
+	if(l.getIsOccupied())
+	    {
+		ret = false;
+	    }
+	else
+	    {
+		ret = true;
+		l.toggleIsOccupied();
+		l.setAffiliation(c);
+	    }
+	return ret;
+    }
+
+    public boolean playStone(Color c, int x, int y)
+    {
+    }
 
 }
