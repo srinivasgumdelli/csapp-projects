37600e63aba245edefd3809b1e7d070f55a1fa8e
heavenslashinghell@gmail.com <heavenslashinghell@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2012-11-07 03:48:40 +0000

    Adding Test Methods for Heisen - more work will have to wait til Thursday.

--- trunk/Go_Project/Heisen.java
+++ trunk/Go_Project/Heisen.java
@@ -16,11 +16,40 @@ public class Heisen{
 		y2 = x2 * w;
 	}
 	
-	public getY1(){
+	public double getY1(){
 		return y1;
 	}
 	
-	public getY2(){
+	public double getY2(){
 		return y2;
+	}
+	
+	public double getX1(){
+		return x1;
+	}
+	
+	public double getX2(){
+		return x2;
+	}
+	
+	public static void main(String[] args){
+		Heisen g = new Heisen();
+		System.out.println(g.getX1() + "\t" + g.getX2());
+		System.out.println(g.getY1() + "\t" + g.getY2());
+		System.out.println(g.getY1() + g.getY2());
+		
+		System.out.println("\n");
+		
+		Heisen g2 = new Heisen();
+		System.out.println(g2.getX1() + "\t" + g2.getX2());
+		System.out.println(g2.getY1() + "\t" + g2.getY2());
+		System.out.println(g2.getY1() + g2.getY2());
+		
+		System.out.println("\n");
+		
+		Heisen g3 = new Heisen();
+		System.out.println(g3.getX1() + "\t" + g3.getX2());
+		System.out.println(g3.getY1() + "\t" + g3.getY2());
+		System.out.println(g3.getY1() + g3.getY2());
 	}
 }
