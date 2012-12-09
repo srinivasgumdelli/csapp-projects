28c11e207542dbb1781d86b7546101f7fbf4e3b1
Maxwizdomer@gmail.com <Maxwizdomer@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2012-12-09 22:14:22 +0000

    update

--- trunk/Go_Project/GoGame.java
+++ trunk/Go_Project/GoGame.java
@@ -4,7 +4,7 @@ public class GoGame
 {
     private Goban _board;
     private int _turns;
-    private int passes;
+    private int _passes;
     private GameTreenode _current;
     private GameTreenode _root;
 
@@ -17,6 +17,7 @@ public class GoGame
     public GoGame()
     {
 	_board = new Goban();
+	_passes = 0;
 	_turns = 0;
 	_root = _current = new GameTreenode();
 	_ko = Korule.SIMPLE; //Ko rule set to simple by default
@@ -32,41 +33,52 @@ public class GoGame
 	    {
 
 		Scanner sc = new Scanner(System.in);
-
+		int x, y;
 
 		System.out.println("Enter X coodinate, enter 9000 to quit, 9001 to pass");
-		int x = sc.nextInt();
-		if(x == 9000)
+		x = sc.nextInt();
+		if(x == 9001)
+		    _passes++;		
+		else if(x == 9000)
 		    break;
-		else if(x == 9001)
-
-		System.out.println("Enter Y coordinate");
-		int y = sc.nextInt();
-		if(_turns%2 == 0)
-		    legalMove = _board.playStone(Color.BLACK, x, y);
 		else
-		    legalMove = _board.playStone(Color.WHITE, x, y);
-		
-		while(legalMove == false)
-		    {	    
-			System.out.println("Illegal Move try again");
-			System.out.println("Enter X coodinate");
-			x = sc.nextInt();
-			if(x == 9000)
-			    break;
+		    _passes = 0;		
+
+		if(_passes == 2)
+		    break;
+
+		if(x != 9001)
+		    {
 			System.out.println("Enter Y coordinate");
 			y = sc.nextInt();
 			if(_turns%2 == 0)
 			    legalMove = _board.playStone(Color.BLACK, x, y);
 			else
-			    legalMove = _board.playStone(Color.WHITE, x, y);   	
+			    legalMove = _board.playStone(Color.WHITE, x, y);
+		    
+			while(legalMove == false)
+			    {	    
+				System.out.println("Illegal Move try again");
+				System.out.println("Enter X coodinate");
+				x = sc.nextInt();
+				if(x == 9000)
+				    break;
+				System.out.println("Enter Y coordinate");
+				y = sc.nextInt();
+				if(_turns%2 == 0)
+				    legalMove = _board.playStone(Color.BLACK, x, y);
+				else
+				    legalMove = _board.playStone(Color.WHITE, x, y);   	
+			    }
 		    }
-
 		_turns++;
 		System.out.println(_board.toString());    
 	    }
-
-
+	if(_passes == 2)
+	    {
+		_board.paintTerritory();
+		System.out.println(_board.toString());
+	    }
     }
 
     public static void main(String[] args)
