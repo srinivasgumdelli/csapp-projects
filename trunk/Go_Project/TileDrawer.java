221be5951fd1a7104bcbf9af110c6d1f5f29e717
heavenslashinghell@gmail.com <heavenslashinghell@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2012-12-09 10:21:59 +0000

    Napping

--- trunk/Go_Project/TileDrawer.java
+++ trunk/Go_Project/TileDrawer.java
@@ -7,6 +7,7 @@
  */
  
  import javax.swing.JPanel;
+ import java.awt.Color;
  import java.awt.*;
 
 public class TileDrawer extends JPanel{
@@ -25,11 +26,26 @@ public class TileDrawer extends JPanel{
     
     public void paint(Graphics graphics){
     	Graphics2D g = (Graphics2D)graphics;
+    	
+    	// drawing the board
+    	Color brown = new Color(156, 93, 82);
+    	g.setColor(brown);
+    	g.fillRect(0, 0, cellSpace*dimension, cellSpace*dimension);
+    	
+    	// drawing the lines
+    	g.setColor(Color.DARK_GRAY);
+    	for (int i = 0; i < dimension; i++){
+    		g.drawLine(i*cellSpace, 0, i*cellSpace, dimension*cellSpace);
+    		g.drawLine(0, i*cellSpace, dimension*cellSpace, i*cellSpace);
+    	}
+    			
+    	
+    	// drawing stones with their color
     	for(int i = 0; i < dimension; i++)
     		for(int j = 0; j < dimension; j++){
     			g.setColor(_board[i][j].getAffiliation());
     			if (_board[i][j].getOccupied())
-    				g.fillOval(dimension - i, dimension - j, StoneSize, StoneSize);
+    				g.fillOval(i*cellSpace, j*cellSpace, StoneSize, StoneSize);
     		}
     }
 }
