a7e60a9e721f9abf52da292de18b9ce4a4ee2a3a
Maxwizdomer@gmail.com <Maxwizdomer@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2012-12-09 21:23:21 +0000

    update

--- trunk/Go_Project/GoGame.java
+++ trunk/Go_Project/GoGame.java
@@ -1,16 +1,78 @@
+import java.util.Scanner;
+
 public class GoGame
 {
     private Goban _board;
     private int _turns;
-    private GameTreenode current;
-    private GameTreenode root;
+    private int passes;
+    private GameTreenode _current;
+    private GameTreenode _root;
+
+    private enum Korule
+    {
+	SIMPLE, POSITIONAL, SITUATIONAL;
+    }
+    private Korule _ko;
 
     public GoGame()
     {
 	_board = new Goban();
-	turns = 0;
-	root = current = new GameTreenode;
+	_turns = 0;
+	_root = _current = new GameTreenode();
+	_ko = Korule.SIMPLE; //Ko rule set to simple by default
+
     }
 
+    public void go()
+    {
+	System.out.println(_board.toString());
+	boolean legalMove;
+
+	while(true)
+	    {
+
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("Enter X coodinate, enter 9000 to quit, 9001 to pass");
+		int x = sc.nextInt();
+		if(x == 9000)
+		    break;
+		else if(x == 9001)
+
+		System.out.println("Enter Y coordinate");
+		int y = sc.nextInt();
+		if(_turns%2 == 0)
+		    legalMove = _board.playStone(Color.BLACK, x, y);
+		else
+		    legalMove = _board.playStone(Color.WHITE, x, y);
+		
+		while(legalMove == false)
+		    {	    
+			System.out.println("Illegal Move try again");
+			System.out.println("Enter X coodinate");
+			x = sc.nextInt();
+			if(x == 9000)
+			    break;
+			System.out.println("Enter Y coordinate");
+			y = sc.nextInt();
+			if(_turns%2 == 0)
+			    legalMove = _board.playStone(Color.BLACK, x, y);
+			else
+			    legalMove = _board.playStone(Color.WHITE, x, y);   	
+		    }
+
+		_turns++;
+		System.out.println(_board.toString());    
+	    }
+
+
+    }
+
+    public static void main(String[] args)
+    {
+	GoGame test = new GoGame();
+	test.go();
+    }
 
 }
--- trunk/Go_Project/Goban.java
+++ trunk/Go_Project/Goban.java
@@ -2,86 +2,255 @@ public class Goban
 {
 
     Location[][] _board;
-
-    public Goban()
+    int _size;
+ 
+    /**
+     * Constructor, _board is initialized to a size that is two units larger
+     * than nessecary.  Only coordinates one through size are visible to user, 
+     * the border is used to make the backend easier to code.
+     */
+    public Goban(int size)
     {
-	_board = new Location[19][19];
+	_size = size;
+	_board = new Location[size + 2][size + 2];
 	for(int i = 0; i < _board.length; i++)
 	    for(int j = 0; j < _board[0].length; j++)
 		_board[i][j] = new Location();
     }
 
- 
-    public boolean checkCapture(int x, int y)
+    public Goban()
     {
-	boolean ret;
-	Color ownColor = _board[x][y].getAffiliation();
-	//System.out.println(ownColor);
-	boolean hasBlankNeighbors = 
-	    !((_board[x][y + 1]).getOccupied() &&
-	      (_board[x + 1][y]).getOccupied() &&
-	      (_board[x][y - 1]).getOccupied() &&
-	      (_board[x - 1][y]).getOccupied());
-	//System.out.println(hasBlankNeighbors);
-	boolean upUnchecked =
-	    (((_board[x][y + 1]).getAffiliation()).equals(ownColor) &&
-	     !((_board[x][y + 1]).getChecked()));
-	boolean rightUnchecked =
-	    (((_board[x + 1][y]).getAffiliation()).equals(ownColor) &&
-	     !((_board[x + 1][y]).getChecked()));
-	boolean downUnchecked =
-	    (((_board[x][y - 1]).getAffiliation()).equals(ownColor) &&
-	     !((_board[x][y - 1]).getChecked()));
-	boolean leftUnchecked =
-	    (((_board[x - 1][y]).getAffiliation()).equals(ownColor) &&
-	     !((_board[x - 1][y]).getChecked()));
-	//System.out.println((upUnchecked || rightUnchecked || downUnchecked || leftUnchecked));
-	if((_board[x][y]).getOccupied())
+	this(19);
+    }
+
+    private boolean checkBounds(int x, int y )
+    {
+	return (x <= _size && x >= 1 && y <= _size && y >= 1); 
+    }
+
+    /**
+     * capture takes in the coordinates of the LAST PIECE PLAYED
+     * and checks the surrounding pieces for any to be removed
+     *
+     * @return true if any pieces are captured at all. 
+     */
+    public boolean capture(int x, int y)
+    {
+	boolean ret = false;
+	boolean temp = false;
+	if(this.checkBounds(x, y))
 	    {
-		if(hasBlankNeighbors)
-		    ret = false;
-		else if(upUnchecked || rightUnchecked || downUnchecked || leftUnchecked)
+		if(this.checkCapture(x, y + 1))
+		    {
+			ret = true;
+			temp = true;
+		    }
+		for(int i = 1; i <= _size; i++)
+		    for(int j = 1; j <= _size; j++)
+				
+			if(_board[i][j].getChecked())
+			    if(temp)
+				this.removeStone(_board[i][j]);	       	
+			    else
+				_board[i][j].setChecked(false);
+		temp = false;
+
+		if(this.checkCapture(x - 1, y))
+		    {
+			ret = true;
+			temp = true;
+		    }
+		for(int i = 1; i <= _size; i++)
+		    for(int j = 1; j <= _size; j++)
+				
+			if(_board[i][j].getChecked())
+			    if(temp)
+				this.removeStone(_board[i][j]);	       	
+			    else
+				_board[i][j].setChecked(false);
+		temp = false;
+
+		if(this.checkCapture(x, y - 1))
+		    {
+			ret = true;
+			temp = true;
+		    }
+		for(int i = 1; i <= _size; i++)
+		    for(int j = 1; j <= _size; j++)
+				
+			if(_board[i][j].getChecked())
+			    if(temp)
+				this.removeStone(_board[i][j]);	       	
+			    else
+				_board[i][j].setChecked(false);
+		temp = false;
+
+		if(this.checkCapture(x + 1, y))
+		    {
+			ret = true;
+			temp = true;
+		    }
+		for(int i = 1; i <= _size; i++)
+		    for(int j = 1; j <= _size; j++)
+				
+			if(_board[i][j].getChecked())
+			    if(temp)
+				this.removeStone(_board[i][j]);	       	
+			    else
+				_board[i][j].setChecked(false);
+		temp = false;
+	    }
+	return ret;
+
+    }
+
+    private void setBorder(Color c)
+    {
+	for(int i = 0; i < _board.length; i++)
+	    {
+		_board[0][i].setOccupied(true);
+		_board[0][i].setAffiliation(c);
+
+		_board[_board.length - 1][i].setOccupied(true);
+		_board[_board.length - 1][i].setAffiliation(c);
+
+		_board[i][0].setOccupied(true);
+		_board[i][0].setAffiliation(c);
+
+		_board[i][_board.length - 1].setOccupied(true);
+		_board[i][_board.length - 1].setAffiliation(c);
+	    }
+    }
+
+    /**
+     * Helper function that returns whether the stone at a particular location
+     * is part of a group that should be captured.  Kept private because it
+     * alters the state of _board without restoring it, which could easily 
+     * be lost track of in other classes.
+     */
+    private boolean checkCapture(int x, int y)
+    {
+	boolean ret = false;
+	if(this.checkBounds(x, y))
+	    {
+
+		Color ownColor = _board[x][y].getAffiliation();
+		if(ownColor == Color.WHITE)
+		    this.setBorder(Color.BLACK);
+		else if(ownColor == Color.BLACK)
+		    this.setBorder(Color.WHITE);
+		//System.out.println(ownColor);
+		boolean hasBlankNeighbors = 
+		    !(
+		      (_board[x][y + 1]).getOccupied() &&
+		      (_board[x + 1][y]).getOccupied() &&
+		      (_board[x][y - 1]).getOccupied() &&
+		      (_board[x - 1][y]).getOccupied()
+		      );
+		//System.out.println(hasBlankNeighbors);
+		
+		boolean upUnchecked, downUnchecked, rightUnchecked, leftUnchecked;	       
+		upUnchecked =
+		    (((_board[x][y + 1]).getAffiliation()).equals(ownColor) 
+		     && !((_board[x][y + 1]).getChecked()));
+		rightUnchecked =
+		    (((_board[x + 1][y]).getAffiliation()).equals(ownColor) 
+		     && !((_board[x + 1][y]).getChecked()));
+		downUnchecked =
+		    (((_board[x][y - 1]).getAffiliation()).equals(ownColor) 
+		     && !((_board[x][y - 1]).getChecked()));
+		leftUnchecked =
+		    (((_board[x - 1][y]).getAffiliation()).equals(ownColor) &&
+		     !((_board[x - 1][y]).getChecked()));
+		//System.out.println((upUnchecked || rightUnchecked || downUnchecked || leftUnchecked));
+		if((_board[x][y]).getOccupied())
 		    {
 			(_board[x][y]).setChecked(true);
-			ret = 
-			    (upUnchecked ? checkCapture(x, y + 1) : true) &&
-			    (rightUnchecked ? checkCapture(x + 1, y) : true) &&
-			    (downUnchecked ? checkCapture(x, y - 1) : true) &&
-			    (leftUnchecked ? checkCapture(x - 1, y) : true);
-		    }
+			if(hasBlankNeighbors)
+			    ret = false;
+			else if(upUnchecked || rightUnchecked || downUnchecked || leftUnchecked)
+			    {
+				ret = 
+				    (upUnchecked ? checkCapture(x, y + 1) : true) &&
+				    (rightUnchecked ? checkCapture(x + 1, y) : true) &&
+				    (downUnchecked ? checkCapture(x, y - 1) : true) &&
+				    (leftUnchecked ? checkCapture(x - 1, y) : true);
+			    }
+			else
+			    ret = true;
+		    }		
 		else
-		    ret = true;
-	    }		
-	else
-	    {
-		System.out.println
-		    ("Warning, checkCapture called on empty location");
-		ret = false;
+		    {
+			System.out.println
+			    ("Warning, checkCapture called on empty location");
+			ret = false;
+		    }
 	    }
+	this.setBorder(Color.NONE);
 	return ret;
     }
-	    
-    public boolean playStone(Color c, Location l)
+
+
+    private void markTerritory(Color c, int x, int y)
     {
-	boolean ret;
-	if(l.getOccupied())
+	boolean valid = (checkBounds(x, y)) && 
+	    (!(_board[x][y].getAffiliation() == c)) && 
+	    (!_board[x][y].getOccupied());
+
+	if(valid)
 	    {
-		ret = false;
+		if(c == Color.DISPUTED)
+		    {
+			_board[x][y].setAffiliation(Color.DISPUTED);
+			this.markTerritory(Color.DISPUTED, x, y + 1);
+			this.markTerritory(Color.DISPUTED, x + 1, y);
+			this.markTerritory(Color.DISPUTED, x, y - 1);
+			this.markTerritory(Color.DISPUTED, x - 1, y);
+		    }
+		else if( _board[x][y].getAffiliation() == Color.NONE)
+		    {
+_board[x][y].setAffiliation(c);
+			this.markTerritory(c , x, y + 1);
+			this.markTerritory(c , x + 1, y);
+			this.markTerritory(c , x, y - 1);
+			this.markTerritory(c , x - 1, y);
+		    }
+		else 
+		    this.markTerritory(Color.DISPUTED, x, y);
 	    }
-	else
-	    {
-		ret = true;
-		l.setOccupied(true);
-		l.setAffiliation(c);
-	    }
-	return ret;
     }
 
     public boolean playStone(Color c, int x, int y)
     {
 	boolean ret;
-	if(x < _board.length && y < _board[0].length)
-	    ret = this.playStone(c, _board[x][y]);
+	boolean suicide;
+	Location l = _board[x][y];
+
+	if(this.checkBounds(x, y))
+	    {
+		if(l.getOccupied())
+		    ret = false;
+		else
+		    {
+			ret = true;
+			l.setOccupied(true);
+			l.setAffiliation(c);
+			suicide = this.checkCapture(x, y);
+			for(int i = 0; i < _board.length; i++)
+			    for(int j = 0; j < _board[0].length; j++)
+				(_board[i][j]).setChecked(false);
+			if(suicide)
+			    {
+				if(!this.capture(x, y))
+				    {
+					this.removeStone(l);
+					ret = false;
+				    }
+				else
+				    ret = true;
+			    }
+		    }
+	    }
 	else 
 	    ret = false;
 	return ret;
@@ -107,7 +276,7 @@ public class Goban
     public boolean removeStone(int x, int y)
     {
 	boolean ret;
-	if(x < _board.length && y < _board[0].length)
+	if(this.checkBounds(x, y))
 	    ret = this.removeStone(_board[x][y]);
 	else
 	    ret = false;
@@ -124,10 +293,18 @@ public class Goban
     public String toString()
     {
 	String ans = "";
-	for(int j = _board[0].length - 1; j >= 0; j--)
+	for(int j = _size - 1; j > 0; j--)
 	    {
-		for(int i = 0; i < _board.length; i++)
-		    ans += _board[i][j].getAffiliation();
+		for(int i = 1; i < _size; i++)
+		    {
+			String temp = "" +  _board[i][j].getAffiliation();
+			if(temp.equals("BLACK"))
+			    ans += " X";
+			else if(temp.equals("WHITE"))
+			    ans += " O";
+			else
+			    ans += " -";
+		    }
 		ans += "\n";
 	    }
 	return ans;
@@ -137,19 +314,19 @@ public class Goban
     {
 	Goban ban = new Goban();
 	System.out.println(ban.toString());
-	ban.playStone(Color.WHITE, 1, 8);
-	ban.playStone(Color.WHITE, 0, 0);
-	ban.playStone(Color.WHITE, 18, 18);
+	ban.playStone(Color.WHITE, 0, 8);
+	ban.playStone(Color.WHITE, 1, 1);
+	ban.playStone(Color.WHITE, 19, 19);
 	ban.playStone(Color.BLACK, 2, 8);
-	ban.playStone(Color.BLACK, 18, 18);
-	ban.playStone(Color.BLACK, 0, 0);
+	ban.playStone(Color.BLACK, 19, 19);
+	ban.playStone(Color.BLACK, 1, 1);
 	System.out.println(ban.toString());
-	if(ban.removeStone(0, 0))
-	    if(ban.removeStone(18, 18))
+	if(ban.removeStone(1, 1))
+	    if(ban.removeStone(19, 19))
 		{
-		    ban.playStone(Color.BLACK, 18, 18);
+		    ban.playStone(Color.BLACK, 19, 19);
 		    System.out.println((ban.getLocation(18,18)).toString());
-		    ban.playStone(Color.BLACK, 0, 0);
+		    ban.playStone(Color.BLACK, 1, 1);
 		    System.out.println(ban.toString());
 		}
 
@@ -160,8 +337,7 @@ public class Goban
 	ban.playStone(Color.BLACK, 10, 9);
 	ban.playStone(Color.BLACK, 9, 10);
 	System.out.println(ban.toString());
-	System.out.println(ban.checkCapture(10, 10));
-	ban.removeStone(10,10);
+	ban.capture(9, 10);	
 	System.out.println(ban.toString());
 
 	ban = new Goban();
@@ -178,8 +354,12 @@ public class Goban
 	ban.playStone(Color.BLACK, 10, 8);
 	ban.playStone(Color.BLACK, 11, 9);
 	ban.playStone(Color.BLACK, 9, 9);
+	
 	System.out.println(ban.toString());
-	System.out.println(ban.checkCapture(10,10));
+	ban.capture(9, 9);	
+	System.out.println(ban.toString());
+	ban.markTerritory(Color.BLACK, 10, 10);
+	System.out.println(ban.toString());
     }//passes tests so far
 
 }
