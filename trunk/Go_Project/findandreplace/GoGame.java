e3e94fc54084b1958fc7e6bf95393e673ac7f17c
Maxwizdomer@gmail.com <Maxwizdomer@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2012-12-14 06:21:59 +0000

    strange gltich

--- trunk/Go_Project/findandreplace/GoGame.java
+++ trunk/Go_Project/findandreplace/GoGame.java
@@ -7,8 +7,11 @@ public class GoGame
     private int _passes;
     private double _komi;
     private boolean _heisen;
+
     private GameTreenode _current;
     private GameTreenode _root;
+
+    private TileDrawer _gui;
 
     private enum Korule
     {
@@ -26,6 +29,118 @@ public class GoGame
 	_root = _current = new GameTreenode();
 	_ko = Korule.SIMPLE; //Ko rule set to simple by default
 
+    }
+
+    public GoGame(TileDrawer drawer)
+    {
+	this();
+	_gui = drawer;
+    }
+
+    public boolean setHeisen(boolean x)
+    {
+	_heisen = x;
+	return _heisen;
+    }
+
+    public void updateGui()
+    {
+	for(int j = _board.getSize(); j > 0; j--)
+	    for(int i = 1; i <= _board.getSize(); i++)
+		_gui.addStone(i, 1 + _board.getSize() - j, 
+			      _board.getLocation(i, j).getAffiliation()); 
+    }
+
+    /**
+     * Fuction to play move called by the gui, normally returns true, returns
+     * false when the game is over.  Takes two ints as parameters, if either are
+     * -1 it will interpret it as a pass.
+     */
+
+    public boolean makeMove(int x, int y)
+    {
+	boolean ans = true;
+	boolean legalMove;
+	Affiliation currentPlayer;
+
+	if(x == -1 || y == -1)
+	    _passes++;
+	else
+	    {
+		if(_turns%2 == 0)
+		    currentPlayer = Affiliation.BLACK;
+		else
+		    currentPlayer = Affiliation.WHITE;
+
+		legalMove = _board.playStone(currentPlayer, x, y);     	
+		if(legalMove)
+		    {
+			updateGui();
+			_passes = 0;
+			_turns++;
+		    }
+	    }
+
+	if(_passes == 2)
+	    ans = false;
+	return ans;
+    }
+
+    /**
+     * HeisenGo function to introduce randomness, takes in a array of 
+     * size 2 {x,y} and a boolean and returns a new array with randomized 
+     * cooordinates.  If the boolean value is true, it will eschew the normal 
+     * selection process and pick a totally random coordinate.  Boolean will be
+     * set to true if is has been called too many times(see makeMove above).
+     */
+    private int[] heisenfy(int[] coord, boolean over)
+    {
+	int[] ans = new int[2];
+	int distance = 0 ;
+	int dx;
+	int dy;
+
+	int x = coord[0];
+	int y = coord[1];
+
+	if(over)
+	    {
+		distance = 0;
+		x = (int)(Math.random()*20);
+		y = (int)(Math.random()*20);
+	    }
+	else
+	    {
+		double temp = (Math.random()*(64.2 + 13.1*4 + 4.2*8 + 0.2*12));
+		if(temp > 64.2 + 13.1*4 + 4.2*8 + 0.2*12 - 0.0001)
+		    {
+			distance = 0;
+			x = (int)(Math.random()*20);
+			y = (int)(Math.random()*20);
+		    }
+		else if(temp > (64.2 + 13.1*4 + 4.2*8))
+		    distance = 3;
+		else if(temp > (64.2 + 13.1*4))
+		    distance = 2;
+		else if(temp > 64.2)
+		    distance = 1;
+		if(distance > 0)
+		    {
+			dx = (int)(Math.random()*distance + 1);
+			dy = distance - dx;
+			if(Math.random() > 0.5)
+			    x = x + dx;
+			else 
+			    x = x - dx;
+			if(Math.random() > 0.5)
+			    y = y + dy;
+			else
+			    y = y - dy;
+		    }
+	    }
+	ans[0] = x;
+	ans[1] = y;
+	return ans;				 			    
     }
 
     public void go()
@@ -129,11 +244,12 @@ public class GoGame
 		System.out.println(_board.toString());    
 	    }
 	if(_passes == 2)
-	    endGame(); 
+	    System.out.println(endGame()); 
     }
 
-    private void endGame()
+    public String endGame()
     {
+	String ans;
 	double whiteScore, blackScore;
 
 	_board.paintTerritory();
@@ -142,13 +258,15 @@ public class GoGame
 	blackScore = _board.countTerritory(Affiliation.BLACK, "Chinese");
 	whiteScore = _board.countTerritory(Affiliation.WHITE, "Chinese") + _komi;
 
-	System.out.printf("Black: %.1f points  White: %.1f points\n", blackScore, whiteScore); 
+	ans  = String.format("Black: %.1f points  White: %.1f points\n", blackScore, whiteScore); 
 
 	if(blackScore > whiteScore)
-	    System.out.println("Black wins!");
+	    ans += ("Black wins!");
 	else
-	    System.out.println("White wins!");	    
+	    ans += ("White wins!");	  
+	return ans;  
     }
+
     public static void main(String[] args)
     {
 	GoGame test = new GoGame();
