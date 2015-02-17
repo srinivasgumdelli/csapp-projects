05a075576a35df74114f52e837a62508587dad99
heavenslashinghell@gmail.com <heavenslashinghell@gmail.com@73c6857d-e18a-d3f8-f453-254b4a726389>
2015-02-17 19:55:48 +0000

    Moving stuff around

--- ShipSim/gun.cpp
+++ ShipSim/gun.cpp
@@ -2,6 +2,16 @@
 
 /***** What follows is the Guns section ****/
 
+Gun::~Gun(){
+	clear();
+}
+
+void clear(){
+	if(next != NULL)
+		next.clear();
+	delete this;
+} /* Correct this! Untested and unreasonable! */
+
 Gun::Gun(const Gun &other){
 	copyB(other);
 }
@@ -16,27 +26,64 @@ void copyB(const Gun &src){
 	loc 		= src.loc;
 	beam		= src.beam;
 	echelon		= src.echelon;
+	spread		= src.spread;
+	maxAngle	= src.maxAngle;
 	g			= src.g;
 	head		= src.head;
+	next		= src.next;
+	
+	Gun *tmp 	= src.next;
+	Gun *cur	= next;
+	while (tmp != NULL){
+		next	= tmp;
+		cur		= cur->next;
+		tmp		= tmp->next;
+	}
 }
 
-float Gun::Dbarb(float barrelMounts, float gunDiameter){
-	float d = gunDiameter / (IN2CM * 10.);
-	return (barrelMounts * (1 + d/2.) + 0.5 * d + 3) / M2FT;
+float Gun::Dbarb(float barrelMounts){
+	float d = g.d / (IN2CM * 10.);
+	return (barrelMounts * (1 + g.d/2.) + 0.5 * g.d + 3) / M2FT;
 }
 
-float Gun::Wshell(float gunYear, float gunDiameter, float gunCaliber){
-	float d 	= gunDiameter / (IN2CM * 10.);
-	float c1 	= gunCaliber > 45. ? 1. + sqrt(gunCaliber - 45.)/45. : 1. - sqrt(45. - gunCaliber)/45.;
-	float c2 	= gunYear < 1890 ? 1. - (1890. - gunYear)/66.66666 : 1.;
-	return pow(d, 3.) * c1 * pow(c2, 2.) / KG2LB;
+float Gun::Wshell(){
+	float d 	= g.d / (IN2CM * 10.);
+	float c1 	= g.L > 45. ? 1. + sqrt(g.L - 45.)/45. : 1. - sqrt(45. - g.L)/45.;
+	float c2 	= g.L < 1890 ? 1. - (1890. - g.L)/66.66666 : 1.;
+	return pow(g.d, 3.) * c1 * pow(c2, 2.) / KG2LB;
 }
 
-float Gun::Wgun(float L, float gd, float bM){
-	float d		= gd / (IN2CM * 10.);
-	float sW 	= Wshell(y, d, L) * KG2LB;
-	return sW * L * (1. + pow(1. / d, 2.3298) / 812.389435 * bM * 3.5 * (1. - 2.163769 * d / 100.) * ST2MT;
+float Gun::Wgun(){
+	float total = 0.0;
+	Mount *cur	= head;
+	while(cur != NULL){
+		total 	+= _Wgun(cur->barrel_ct);
+		cur		= cur->next;
+	}
+	return total;
+}
+
+// Helper function
+float Gun::_Wgun(unsigned int bM){
+	float d		= g.d / (IN2CM * 10.);
+	float sW 	= Wshell() * KG2LB;
+	return sW * g.L * (1. + pow(1. / g.d, 2.3298) / 812.389435 * bM * 3.5 * (1. - 2.163769 * g.d / 100.) * ST2MT;
 }
 
 float Gun::Wammo(){
+	float total = 0.0;
+	Mount *cur	= head;
+	while(head != NULL){
+		total 	+= cur->barrel_ct;
+		cur 	= cur->next;
+	}
+	return total * g.SpB * Wshell();
+}
+
+shell Gun::Range(){
+	shell *tmp { g.d, Wshell(), 0.25, 0.3, maxAngle, mV(g.d, g.L, Wshell(), g.Y), 0.1, 0. };
+	float step 	= 1000000.;
+	for(int i = 0; tmp->h > 0; i++)
+		tmp->d += ballistics(i/step, tmp);
+	return tmp;
 }
--- ShipSim/gun.h
+++ ShipSim/gun.h
@@ -2,42 +2,56 @@
 #define GUN_H
 
 #include "enumparser.h"
+#include "main.h"
+#include "ballistic.h"
 
 class Gun{
 	public:	
-		Battery(placement _l, bool _b, bool _e, float _s, gunData _g, mount _m) : loc(_l), beam(_b), echelon(_e), spread(_s), g(_g), head(_m), next(NULL);
-		
-		~Battery();
-		Battery(const Battery &other);
-		const Battery &operator=(const Battery &src);
-		
-		float BarbetteDiameter(float barrelMounts, float gunDiameter);
-		float Shellweight(float gunYear, float gunDiameter, float gunCaliber);
-		float Wammo();
+		struct Data{
+			float d;
+			float L;
+			//float W;	// not in current use
+			int	  Y;
+			float SpB;
+		};
 	
+		Gun(placement _l, bool _b, bool _e, float _s, gunData _g, mount _m) : loc(_l), beam(_b), echelon(_e), spread(_s), g(_g), head(_m), next(NULL);
+		
+		~Gun();
+		Gun(const Gun &other);
+		const Gun &operator=(const Gun &src);
+		
+		float Dbarb(float barrelMounts);
+		float Wshell();
+		float Wgun();
+		float _Wgun(unsigned int bM);
+		float Wammo();
+		shell Range();
+
 		Placement 		loc;
 		bool 			beam;
 		bool			echelon;
 		float			spread;	
-		extern Data		g;
+		float			maxAngle;
+		Data			g;
 		extern Mount	*head;
+		Gun				*next;
 	private:
-		void copyB(const Battery &src);
+		void copyB(const Gun &src);
+		void clear();
 		
 		class Mount{
 			public:
+				Mount() : barrel_ct(0), mount_H(0), next(NULL);
+				inline ~Mount(){
+					delete next;
+				} /* Correct this! Untested and unreasonable! */
+			
 				unsigned int 	barrel_ct;
 				unsigned int 	mount_H;	// height relative to deck
-				MountType 		mT;
+				//MountType 		mT;		// currently assume Turret
 				Mount 			*next;
 			private:
-		};
-		class Data{
-			float d;
-			float L;
-			float W;
-			float Y;
-			float SpB;
 		};
 };
 
